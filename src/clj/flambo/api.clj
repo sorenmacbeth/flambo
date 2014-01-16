735809bb2d21801905857714f60ce1ce9d966346
jimdunn <jdunn@yieldbot.com>
2014-01-16 14:52:15 -0700

    meow

--- src/clj/flambo/api.clj
+++ src/clj/flambo/api.clj
@@ -47,9 +47,8 @@
 (defn text-file [spark-context filename]
   (.textFile spark-context filename))
 
-(defn parallelize
-  ([spark-context lst] (.parallelize spark-context lst))
-  #([spark-context lst num-slices] (.parallelize spark-context lst num-slices))
+(defn parallelize  [spark-context lst] (.parallelize spark-context lst)
+  
   )
 
 ;;; Transformations
