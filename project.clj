6672c54532178d3c709565ef42ad6f9a151395ee
Soren Macbeth <soren@dopeness.org>
2014-02-15 17:40:37 -0800

    merge thing

--- project.clj
+++ project.clj
@@ -11,14 +11,18 @@
                  [org.clojure/tools.logging "0.2.6"]
                  [yieldbot/serializable-fn "0.0.4"]
                  [com.twitter/carbonite "1.3.3"]
-                 [com.twitter/chill_2.9.3 "0.3.5"]]
+                 [com.twitter/chill_2.10 "0.3.5"]]
   :plugins [[s3-wagon-private "1.1.2"]]
   :profiles {:provided
              {:dependencies
-              [[org.apache.spark/spark-core_2.9.3 "0.8.1-incubating"]]}}
+              [[org.apache.spark/spark-core_2.10 "0.9.0-incubating"]]}
+             :1.6
+             {:dependencies [[org.clojure/clojure "1.6.0-beta1"]]}}
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
   :javac-options ["-source" "1.6" "-target" "1.6"]
-  :aot [flambo.function])
+  :global-vars {*warn-on-reflection* true}
+  :aot [flambo.function]
+  )
 (cemerick.pomegranate.aether/register-wagon-factory!
  "s3p" #(eval '(org.springframework.aws.maven.PrivateS3Wagon.)))
