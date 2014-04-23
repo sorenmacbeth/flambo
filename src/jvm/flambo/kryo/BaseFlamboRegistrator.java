2778dfcea97a4c55a251a562fb449a2bf408732b
Soren Macbeth <soren@dopeness.org>
2014-04-23 08:08:59 -0700

    clean up

--- src/jvm/flambo/kryo/BaseFlamboRegistrator.java
+++ src/jvm/flambo/kryo/BaseFlamboRegistrator.java
@@ -6,14 +6,19 @@ import carbonite.JavaBridge;
 import scala.Tuple2;
 import com.twitter.chill.Tuple2Serializer;
 
-public class BaseFlamboRegistrator implements KryoRegistrator {
+public class FlamboRegistrator extends Registrator {
+
+  protected void register(Kryo kryo) {
+  }
 
   @Override
   public final void registerClasses(Kryo kryo) {
-    CarboniteRegistrator.registerCarbonite(kryo);
-  }
-
-  protected void register(Kryo kryo) {
-    // subclasses should override this
+    try {
+      JavaBridge.enhanceRegistry(kryo);
+      kryo.register(Tuple2.class, new Tuple2Serializer());
+      register(kryo);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to register kryo!");
+    }
   }
 }
