e03284dbd16f7a41c804f647862753ae37448331
Soren Macbeth <soren@dopeness.org>
2014-05-03 12:55:55 -0700

    beef up conf namespace

--- src/clj/flambo/conf.clj
+++ src/clj/flambo/conf.clj
@@ -1,6 +1,6 @@
 (ns flambo.conf
   (:import [org.apache.spark SparkConf])
-  (:refer-clojure :exclude (set)))
+  (:refer-clojure :exclude (set get contains)))
 
 ;; SparkConf functions
 
@@ -16,8 +16,45 @@
 (defn jars [conf jars]
   (.setJars conf (into-array String jars)))
 
-(defn set [conf key val]
-  (.set conf key val))
+(defn set
+  ([conf key val]
+     (.set conf key val))
+  ([conf amap]
+     (loop [c conf
+            aseq (seq amap)]
+       (if aseq
+         (let [[k v] (first aseq)
+               c (set c k v)]
+           (recur c (next aseq)))
+         c))))
+
+(defn set-if-missing [conf key val]
+  (.setIfMissing key val))
+
+(defn set-executor-env
+  ([conf key val]
+     (.setExecutorEnv conf key val))
+  ([conf amap]
+     (loop [c conf
+            aseq (seq amap)]
+       (if aseq
+         (let [[k v] (first aseq)
+               c (set-executor-env c k v)]
+           (recur c (next aseq)))
+         c))))
+
+(defn remove [conf key]
+  (.remove conf key))
+
+(defn get
+  ([conf key]
+     (.get conf key))
+  ([conf key default]
+     (.get conf key default)))
+
+(defn get-all [conf]
+  (into {} (for [t (.getAll conf)]
+             {(._1 t) (._2 t)})))
 
 (defn spark-home [conf home]
   (.setSparkHome conf home))
