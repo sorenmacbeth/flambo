4c9ed056da12c209c3d1b17abcb5f8352db4b64c
Muslim Baig <mbaig@yieldbot.com>
2014-06-18 18:31:05 -0400

    Some api tests for flambo, more to come

--- test/flambo/api_test.clj
+++ test/flambo/api_test.clj
@@ -49,3 +49,121 @@
   "double untuple returns a vector with a key and a 2 vector value"
   (let [double-tuple2 (scala.Tuple2. 1 (scala.Tuple2. 2 "hi"))]
     (f/double-untuple double-tuple2) => [1 [2 "hi"]])))
+
+(facts
+  "about transformations"
+
+  (let [conf (-> (conf/spark-conf)
+                 (conf/master "local[*]")
+                 (conf/app-name "api-test"))]
+    (f/with-context c conf
+      (fact
+        "map returns an RDD formed by passing each element of the source RDD through a function"
+        (-> (f/parallelize c [1 2 3 4 5])
+            (f/map (f/sparkop [x] (* 2 x)))
+            f/collect
+            vec) => [2 4 6 8 10])
+
+      (fact
+        "map-to-pair returns an RDD of (K, V) pairs formed by passing each element of the source
+        RDD through a pair function"
+        (-> (f/parallelize c ["a" "b" "c" "d"])
+            (f/map-to-pair (f/sparkop [x] [x 1]))
+            (f/map f/untuple)
+            f/collect
+            vec) => [["a" 1] ["b" 1] ["c" 1] ["d" 1]])
+
+      (fact
+        "reduce-by-key returns an RDD of (K, V) when called on an RDD of (K, V) pairs"
+        (-> (f/parallelize c [["key1" 1]
+                              ["key1" 2]
+                              ["key2" 3]
+                              ["key2" 4]
+                              ["key3" 5]])
+            (f/reduce-by-key (f/sparkop [x y] (+ x y)))
+            f/collect
+            vec) => (contains #{["key1" 3] ["key2" 7] ["key3" 5]}))
+
+      (fact
+        "similar to map, but each input item can be mapped to 0 or more output items;
+        mapping function must therefore return a sequence rather than a single item"
+        (-> (f/parallelize c ["Four score and seven years ago our fathers"
+                              "brought forth on this continent a new nation"])
+            (f/flat-map (f/sparkop [x] (clojure.string/split x #" ")))
+            f/collect
+            vec) => ["Four" "score" "and" "seven" "years" "ago" "our" "fathers" "brought" "forth" "on" "this" "continent" "a" "new" "nation"])
+
+      (fact
+        "filter returns an RDD formed by selecting those elements of the source on which func returns true"
+        (-> (f/parallelize c [1 2 3 4 5 6])
+            (f/filter (f/sparkop [x] (even? x)))
+            f/collect
+            vec) => [2 4 6])
+
+      (fact
+        "join returns an RDD of (K, (V, W)) pairs with all pairs of elements of each key when called on RDDs of type (K, V) and (K, W)"
+        (let [LDATA (f/parallelize c [["key1" [2]]
+                                      ["key2" [3]]
+                                      ["key3" [5]]
+                                      ["key4" [1]]
+                                      ["key5" [2]]])
+              RDATA (f/parallelize c [["key1" [22]]
+                                      ["key3" [33]]
+                                      ["key4" [44]]])
+              ]
+          (-> (f/join LDATA RDATA)
+              f/collect
+              vec)) => [["key3" [[5] [33]]]
+                        ["key4" [[1] [44]]]
+                        ["key1" [[2] [22]]]])
+
+      (fact
+        "left-outer-join returns an RDD of (K, (V, W)) when called on RDDs of type (K, V) and (K, W)"
+        (let [LDATA (f/parallelize c [["key1" [2]]
+                                      ["key2" [3]]
+                                      ["key3" [5]]
+                                      ["key4" [1]]
+                                      ["key5" [2]]])
+              RDATA (f/parallelize c [["key1" [22]]
+                                      ["key3" [33]]
+                                      ["key4" [44]]])]
+          (-> (f/left-outer-join LDATA RDATA)
+              f/collect
+              vec)) => [["key3" [[5] [33]]]
+                        ["key4" [[1] [44]]]
+                        ["key5" [[2] nil]]
+                        ["key1" [[2] [22]]]
+                        ["key2" [[3] nil]]])
+
+      #_(fact
+        ""
+        (-> (f/parallelize c ["Four score and seven years ago"])
+            (f/flat-map-to-pair (f/sparkop [x] (map (fn [x] [x 1])
+                                                    (clojure.string/split x #" "))))
+            (f/map f/double-untuple)
+            f/collect
+            vec) => [["Four" 1] ["score" 1] ["and" 1] ["seven" 1] ["years" 1] ["ago" 1]])
+      )))
+
+(facts
+  "about actions"
+
+  (let [conf (-> (conf/spark-conf)
+                 (conf/master "local[*]")
+                 (conf/app-name "api-test"))]
+    (f/with-context c conf
+      (fact
+        "aggregates elements of RDD using a function that takes two arguments and returns one,
+        return type is a value"
+        (-> (f/parallelize c [1 2 3 4 5])
+            (f/reduce (f/sparkop [x y] (+ x y)))) => 15)
+
+      (fact
+        "count-by-key returns a hashmap of (K, int) pairs with the count of each key; only available on RDDs of type (K, V)"
+        (-> (f/parallelize c [["key1" 1]
+                              ["key1" 2]
+                              ["key2" 3]
+                              ["key2" 4]
+                              ["key3" 5]])
+            (f/count-by-key)) => {"key1" 2, "key2" 2, "key3" 1})
+      )))
