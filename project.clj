4c0f560a04dc27a2a5812073d2206d509e8466f9
Soren Macbeth <soren@dopeness.org>
2014-07-16 15:19:42 -0700

    bump to 0.3.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yieldbot/flambo "0.3.2"
+(defproject yieldbot/flambo "0.3.3-SNAPSHOT"
   :description "A Clojure DSL for Apache Spark"
   :url "https://github.com/yieldbot/flambo"
   :license {:name "Eclipse Public License"
