42b518e7c4c73798f63ddc7ab5f858040c483af4
Soren Macbeth <soren@dopeness.org>
2015-02-17 14:04:16 -0800

    be less fancy

--- src/clojure/flambo/tuple.clj
+++ src/clojure/flambo/tuple.clj
@@ -3,35 +3,15 @@
   (:require [serializable.fn :as sfn])
   (:import [scala Tuple2]))
 
-(defprotocol ClojureTuple2
-  (first [this])
-  (second [this])
-  (vec [this])
-  (seq [this]))
-
-(extend-type Tuple2
-  ClojureTuple2
-  (first [this]
-    (._1 this))
-  (second [this]
-    (._2 this))
-  (vec [this]
-    (let [v (transient [])]
-      (conj! v (first this))
-      (conj! v (second this))
-      (persistent! v)))
-  (seq [this]
-    (clojure.core/seq (vec this))))
-
 (defn tuple [k v]
   (Tuple2. k v))
 
 (defn key-val-fn [f]
   (sfn/fn [^Tuple2 t]
-    (f (first t) (second t))))
+    (f (._1 t) (._2 t))))
 
 (defn key-val-val-fn [f]
   (sfn/fn [^Tuple2 t]
-    (let [k (first t)
-          v ^Tuple2 (second t)]
-      (f k (first v) (second v)))))
+    (let [k (._1 t)
+          v ^Tuple2 (._2 t)]
+      (f k (._1 v) (._2 v)))))
