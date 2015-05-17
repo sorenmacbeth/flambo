e49b2fe61a853c73fde1666d1935c75aa1348fa0
Soren Macbeth <soren@dopeness.org>
2015-05-17 16:44:41 -0700

    bump serializable-fn

--- src/java/flambo/kryo/BaseFlamboRegistrator.java
+++ src/java/flambo/kryo/BaseFlamboRegistrator.java
@@ -4,7 +4,9 @@ import com.esotericsoftware.kryo.Kryo;
 import org.apache.spark.serializer.KryoRegistrator;
 import carbonite.JavaBridge;
 import scala.Tuple2;
+import scala.Tuple3;
 import com.twitter.chill.Tuple2Serializer;
+import com.twitter.chill.Tuple3Serializer;
 
 public class BaseFlamboRegistrator implements KryoRegistrator {
 
@@ -16,27 +18,10 @@ public class BaseFlamboRegistrator implements KryoRegistrator {
     try {
       JavaBridge.enhanceRegistry(kryo);
       kryo.register(Tuple2.class, new Tuple2Serializer());
-
-      // we have to reflect this scala class since it's private wheeee
-      // and grouping returns these, will be fixed in spark 1.0.1
-
-      // Class cls = Class.forName("scala.collection.convert.Wrappers$IterableWrapper");
-      // kryo.register(cls, new JavaIterableWrapperSerializer());
+      kryo.register(Tuple3.class, new Tuple3Serializer());
 
       register(kryo);
 
-      /*
-        We do this because under mesos these serializers don't get registered
-        in the executors like they should and do in the driver which leads to
-        kryo class ID mismatches. Forcing the registration here works around the
-        problem.
-      */
-
-      // kryo.register(scala.collection.convert.Wrappers.IteratorWrapper.class);
-      // kryo.register(scala.collection.convert.Wrappers.SeqWrapper.class);
-      // kryo.register(scala.collection.convert.Wrappers.MapWrapper.class);
-      // kryo.register(scala.collection.convert.Wrappers.JListWrapper.class);
-      // kryo.register(scala.collection.convert.Wrappers.JMapWrapper.class);
 
     } catch (Exception e) {
       throw new RuntimeException("Failed to register kryo!");
