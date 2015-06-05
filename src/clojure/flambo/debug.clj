4b9b6dd7cee2ff82e207219ac89690f466445252
Soren Macbeth <soren@dopeness.org>
2015-06-04 20:54:30 -0700

    make inspect more flexible

--- src/clojure/flambo/debug.clj
+++ src/clojure/flambo/debug.clj
@@ -2,16 +2,20 @@
   (:require [flambo.api :as f]
             [clojure.tools.logging :as log]))
 
-(defn inspect [rdd name]
-  (let [cached (-> rdd
-                   (f/cache)
-                   (f/rdd-name name))]
+(defn inspect [rdd name & {:keys [count? cache?]
+                           :or {:count? false
+                                :cache? false}}]
+  (let [rdd (if cache?
+              (-> rdd
+                  (f/cache)
+                  (f/rdd-name name))
+              rdd)]
     (try
-      (log/info name "partition count:" (f/partition-count cached))
-      (log/info name "partitioner:" (f/partitioner cached))
-      (catch Throwable t))
-    (let [c (f/count cached)]
-      (log/info name "item count:" c)
-      (when-not (zero? c)
-        (log/info name "first item:" (f/first cached))))
-    cached))
+      (log/info name "partition count:" (f/partition-count rdd))
+      (log/info name "partitioner:" (f/partitioner rdd))
+      (catch Exception e))
+    (when count?
+      (let [c (f/count rdd)]
+        (log/info name "item count:" c)))
+    (log/info name "first item:" (f/first rdd))
+    rdd))
