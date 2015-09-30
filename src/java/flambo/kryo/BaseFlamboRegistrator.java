03fefa4ab52e03a68eee34f3b2286ea4f749a96f
Soren Macbeth <soren@dopeness.org>
2015-09-29 21:35:46 -0700

    serialize broadcast and accumulator

--- src/java/flambo/kryo/BaseFlamboRegistrator.java
+++ src/java/flambo/kryo/BaseFlamboRegistrator.java
@@ -1,12 +1,15 @@
 package flambo.kryo;
 
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.serializers.JavaSerializer;
 import org.apache.spark.serializer.KryoRegistrator;
 import carbonite.JavaBridge;
 import scala.Tuple2;
 import scala.Tuple3;
 import com.twitter.chill.Tuple2Serializer;
 import com.twitter.chill.Tuple3Serializer;
+import org.apache.spark.broadcast.Broadcast;
+import org.apache.spark.Accumulator;
 
 public class BaseFlamboRegistrator implements KryoRegistrator {
 
@@ -19,6 +22,8 @@ public class BaseFlamboRegistrator implements KryoRegistrator {
       JavaBridge.enhanceRegistry(kryo);
       kryo.register(Tuple2.class, new Tuple2Serializer());
       kryo.register(Tuple3.class, new Tuple3Serializer());
+      kryo.register(Broadcast.class, new JavaSerializer());
+      kryo.register(Accumulator.class, new JavaSerializer());
 
       register(kryo);
 
