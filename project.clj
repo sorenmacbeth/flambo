bdbd84f6b713e45c97cca735a17443e590b3a8bb
Soren Macbeth <soren@dopeness.org>
2016-07-27 23:41:56 -0700

    update to 1.6.2

--- project.clj
+++ project.clj
@@ -28,11 +28,11 @@
                     flambo.example.tfidf]}
              :provided
              {:dependencies
-              [[org.apache.spark/spark-core_2.10 "1.6.1"]
-               [org.apache.spark/spark-streaming_2.10 "1.6.1"]
-               [org.apache.spark/spark-streaming-kafka_2.10 "1.6.1"]
-               [org.apache.spark/spark-streaming-flume_2.10 "1.6.1"]
-               [org.apache.spark/spark-sql_2.10 "1.6.1"]
+              [[org.apache.spark/spark-core_2.10 "1.6.2"]
+               [org.apache.spark/spark-streaming_2.10 "1.6.2"]
+               [org.apache.spark/spark-streaming-kafka_2.10 "1.6.2"]
+               [org.apache.spark/spark-streaming-flume_2.10 "1.6.2"]
+               [org.apache.spark/spark-sql_2.10 "1.6.2"]
                [com.databricks/spark-csv_2.10 "1.4.0"]]}
              :clojure-1.6
              {:dependencies [[org.clojure/clojure "1.6.0"]]}
