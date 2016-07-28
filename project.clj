b66a1c27fca39da7a5c3ad981c629bc43f801618
Soren Macbeth <soren@dopeness.org>
2016-07-27 23:45:53 -0700

    release version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yieldbot/flambo "0.7.2-SNAPSHOT"
+(defproject yieldbot/flambo "0.7.2"
   :description "A Clojure DSL for Apache Spark"
   :url "https://github.com/yieldbot/flambo"
   :license {:name "Eclipse Public License"
